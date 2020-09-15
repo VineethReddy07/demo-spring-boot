397f6b5764b984fda4e50493c5a01980ba386007
saivineethjataprolu <saivineeth261@gmail.com>
2020-09-15 01:29:41 -0700

    Sample Spring Boot project

---
+++ src/main/java/com/practise/spring/boot/application/demospringboot/controller/PlayersController.java
@@ -0,0 +1,17 @@
+package com.practise.spring.boot.application.demospringboot.controller;
+
+import com.practise.spring.boot.application.demospringboot.service.Players;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PlayersController {
+
+    @GetMapping("players")
+    public List<Players> getDetails(){
+        List<Players> playersList=List.of(new Players(7,"M.S.Dhoni","WK-Batsman"));
+        return playersList;
+    }
+}
---
+++ src/test/java/com/practise/spring/boot/application/demospringboot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.spring.boot.application.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
