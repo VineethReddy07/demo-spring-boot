4607ddf73c21e57598568b12bb3e9905fce77e57
saivineethjataprolu <saivineeth261@gmail.com>
2020-09-15 14:16:00 -0700

    Time and Date With HelloWorld

--- src/main/java/com/practise/spring/boot/application/demospringboot/controller/PlayersController.java
+++ src/main/java/com/practise/spring/boot/application/demospringboot/controller/PlayersController.java
@@ -4,6 +4,7 @@ import com.practise.spring.boot.application.demospringboot.service.Players;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -13,5 +14,10 @@ public class PlayersController {
     public List<Players> getDetails(){
         List<Players> playersList=List.of(new Players(7,"M.S.Dhoni","WK-Batsman"));
         return playersList;
+    }
+
+    @GetMapping("/HelloWorld")
+    public String print(){
+        return "HelloWorld" + LocalDateTime.now();
     }
 }
