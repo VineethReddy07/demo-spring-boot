a2a8f5e2305f67bac2706b5dce14fdf87438fd97
saivineethjataprolu <saivineeth261@gmail.com>
2020-09-17 01:03:41 -0700

    Retreive Data from H2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into person(ID,NAME,LOCATION,BIRTH_DATE)
+VALUES(07,'M.S.DHONI','RANCHI',sysdate());
+insert into person(ID,NAME,LOCATION,BIRTH_DATE)
+VALUES(41,'VINEETH','HYDERABAD',sysdate());
