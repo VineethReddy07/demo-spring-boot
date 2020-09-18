10a84989ef7be414a6d16ec0ae4b44e983afa8a3
saivineethjataprolu <saivineeth261@gmail.com>
2020-09-17 20:51:56 -0700

    Retreive Data from H2 database

--- src/main/java/com/practise/spring/boot/application/demospringboot/controller/SampleController.java
+++ src/main/java/com/practise/spring/boot/application/demospringboot/controller/SampleController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SampleController {
@@ -20,9 +22,36 @@ public class SampleController {
         return "HelloWorld" + " " + LocalDate.now();
     }
 
-    @GetMapping("retrievePlayerProfile")
+    @GetMapping("retrievePlayersProfile")
     public List<Players> dataRetrieve() {
         return service.retrievePlayerProfile();
+    }
+
+    @GetMapping("retrievePlayerInfo")
+    public Players infoRetrieve() {
+        return service.findById
+                (service.retrievePlayerProfile().stream().filter(players ->
+                        players.getId() == 41).collect(Collectors.toList()).get(0).getId());
+    }
+
+    @GetMapping("deleteInfo")
+    public int deleteInfo() {
+        return service.deleteById
+                (service.retrievePlayerProfile().stream().filter(players ->
+                        players.getId() == 07).collect(Collectors.toList()).get(0).getId());
+
+    }
+
+    @GetMapping("insertInfo")
+    public int insertInfo() {
+        return service.insertInfo
+                (new Players(6, "Ravinder", "Hyderabad", new Date()));
+    }
+
+    @GetMapping("updateInfo")
+    public int updateInfo() {
+        return service.update
+                (new Players(6, "Anita", "Hyderabad", new Date()));
     }
 
 }
--- src/main/java/com/practise/spring/boot/application/demospringboot/dataService/PlayerProfileJdbcService.java
+++ src/main/java/com/practise/spring/boot/application/demospringboot/dataService/PlayerProfileJdbcService.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Component
@@ -14,7 +15,35 @@ public class PlayerProfileJdbcService {
     JdbcTemplate jdbcTemplate;
 
     public List<Players> retrievePlayerProfile() {
-        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper(Players.class));
+        return jdbcTemplate.query
+                ("select * from person", new BeanPropertyRowMapper<Players>(Players.class));
 
+    }
+
+    public Players findById(int id) {
+        return jdbcTemplate.queryForObject
+                ("select * from person where id=?", new Object[]{id},
+                        new BeanPropertyRowMapper<Players>(Players.class));
+    }
+
+    public int deleteById(int id) {
+        return jdbcTemplate.update
+                ("delete from person where id=?", new Object[]{id});
+    }
+
+    public int insertInfo(Players players){
+        return jdbcTemplate.update
+                (" insert into person(id,name,location,birth_date) "
+                 + " values(?,?,?,?) ",
+                 new Object[]{players.getId(),players.getName(),
+                 players.getLocation(),new Timestamp(players.getBirthDate().getTime())});
+    }
+
+    public int update(Players players){
+        return jdbcTemplate.update
+                (" update person " +
+                " set name=?, location=?, birth_date=? "
+                + " where id=? ",
+                        new Object[]{players.getName(),players.getLocation(),players.getBirthDate(),players.getId()});
     }
 }
